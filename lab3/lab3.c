b9bb0fe8d6556db700166b0a2cb2ed1319c35257
Binh Minh Nguyen <bmn4@illinois.edu>
2022-02-10 11:44:10 -0600

    Did lab 3 and edited .gitignore to ignore scratchSpace folder

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -14,16 +14,28 @@
 int main()
 {
     // Declare variables
-
+    int n, w1, w2;
+        
     // Prompt user for input
-
+    printf("Enter the values of n, omega1, and omega2 in that order\n");
+    
     // Get user input
-
+    scanf("%d %d %d", &n, &w1, &w2);
+    
     // Compute function output
     /* for i from 0 to n-1
      *     compute and print xi and f(xi)
      *     use sin() function from math.h
      */
+    
+    for(int i = 0; i < n; i++) {
+        double xi = M_PI / n;
+        xi = xi * i;
+
+        double fi = sin(w1*xi) + 0.5 * sin(w2*xi);
+
+        printf("(%lf, %lf)\n", xi, fi);
+    }
 
     return 0;
 }

5a05df63a94b90e82767e9e17f9ea0b2f88ac7fe
Binh Minh Nguyen <bmn4@illinois.edu>
2022-02-18 15:49:09 -0600

    Finished lab4

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -3,7 +3,18 @@
 
 int main(){
   // Write the code to take a number n from user and print all the prime numbers between 1 and n
+  int n;
 
+  printf("Enter value of n: ");
+  scanf("%d", &n);
+
+  printf("Printing primes less than or equal of %d:\n", n);
+
+  for(int i = 1; i <= n; i++) {
+    if(is_prime(i)){
+      printf("%d\n", i);
+    }
+  } 
 
   return 0;
 }
--- lab4/prime.c
+++ lab4/prime.c
@@ -1,7 +1,16 @@
 #include "prime.h"
+#include <math.h>
 
 int is_prime(int n){
   // Return 1 if n is a prime, and 0 if it is not
-  return 0;
+  if(n<=1) {return 0;} // Check for 1, 0, or negative
+
+  for(int i = 2; i < sqrt(n); i++) {
+    if(n % i == 0) {
+      return 0;
+    }
+  }
+
+  return 1;
 }
 

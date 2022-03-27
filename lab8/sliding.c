952c5faa4a11a75c77cd02ca0c597c3fbe14e824
Binh Minh Nguyen <bmn4@illinois.edu>
2022-03-26 21:59:43 -0500

    Finished lab8

--- lab8/sliding.c
+++ lab8/sliding.c
@@ -2,6 +2,24 @@
 /*  Slide all values of array up
 */
 void slide_up(int* my_array, int rows, int cols){
+    // In each col
+    for(int n = 0; n < cols; n++) {
+        // Check each row
+        for(int m = 0; m < rows; m++) {
+            
+            // If cell is not empty
+            if(my_array[m*cols + n] != -1) {
+                // Find first empty cell above it (if it exists)
+                for(int i = 0; i < m; i++) {
+                    // If it does, move the number into that spot
+                    if(my_array[i*cols + n] == -1) {
+                        my_array[i*cols + n] = my_array[m*cols + n];
+                        my_array[m*cols + n] = -1;
+                    }
+                }
 
+            }
+        }
+    }
     return;
 }

588de43e0f5599f6f58c734164e6bdcfd2ca0070
Binh Minh Nguyen <bmn4@illinois.edu>
2022-03-31 15:14:29 -0500

    Did Lab9

--- lab9/vector.c
+++ lab9/vector.c
@@ -4,25 +4,55 @@
 
 vector_t * createVector(int initialSize)
 {
+    vector_t *vector = malloc(sizeof(vector));
+
+    vector->size = 0;
+    vector->maxSize = initialSize;
+
+    vector->array = calloc(initialSize, sizeof(int));
+
+    return vector;
 }
 
 void destroyVector(vector_t * vector)
 {
+    free(vector->array);
+    free(vector);
 }
 
 void resize(vector_t * vector)
 {
+    vector->array = realloc(vector->array, vector->maxSize * 2);
+    vector->maxSize *= 2;
 }
 
 void push_back(vector_t * vector, int element)
 {
+    // If vector is full, resize vector
+    if(vector->size == vector->maxSize) {
+        resize(vector);
+    }
+
+    vector->array[vector->size] = element;
+    vector->size++;
 }
 
 int pop_back(vector_t * vector)
 {
+    // If no items, return 0
+    if(vector->size == 0) {
+        return 0;
+    }
+
+    // Decrement size
+    vector->size--;
+
+    // Return item corresponding to preious size
+    return vector->array[vector->size];
 }
 
 int access(vector_t * vector, int index)
 {
+    return vector->array[index];
 }
 
